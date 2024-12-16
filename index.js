c7f1d61d827f003a997ae889d348c79c86d0e7b3
Mateusz Górniak <matilokop@outlook.com>
2024-12-16 21:29:20 +0100

    Added database integration and users table

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const mysql = require('mysql2');
 
 const app = express();
 
 app.set('views', path.join(__dirname, "/views"));
 app.set('view engine', 'hbs');
 
+const db = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "password",
+	database: "zse_tai3"
+});
+
+db.connect((err) => {
+	if (err) throw err;
+	console.log("Database connected!");
+});
+
 app.get('/', (req, res) => {
-	res.render('home');
+	db.query("SELECT `surname`, `name`, `email` FROM cmsUsers ORDER BY `surname` ASC;", (err, rows) => {
+		if (err) throw err;
+		
+		res.render('home', {
+			users: rows
+		});
+	});
 });
 
 app.listen(3000, (err) => {
